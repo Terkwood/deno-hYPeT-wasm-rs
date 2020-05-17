0da72d58e0288f301408bc119aa98673baa79bd4
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-17 11:34:03 -0400

    Clean up build script

--- build.ts
+++ build.ts
@@ -1,16 +1,25 @@
-#!/usr/bin/env -S deno run --allow-run
+#!/usr/bin/env -S deno run --allow-run --allow-env
 
 let cargoBuild = Deno.run(
-  { "cmd": ["cargo", "build", "--target", "wasm32-unknown-unknown"] },
+  {
+    "cmd": [
+      `cargo`,
+      "build",
+      "--target",
+      "wasm32-unknown-unknown",
+    ],
+  },
 );
 
 await cargoBuild;
 
+let { code } = await cargoBuild.status();
+
 await Deno.run(
   {
     "cmd": [
-      "wasm-gc",
-      "target/wasm32-unknown-unknown/debug/deno_h_ype_t_rs.wasm",
+      `wasm-gc`,
+      `./target/wasm32-unknown-unknown/debug/deno_h_ype_t_rs.wasm`,
     ],
   },
 );
