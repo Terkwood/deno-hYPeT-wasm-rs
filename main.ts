35cf4dd478655e2bdee023ebbb285f7e82252a4c
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-05-17 11:10:17 -0400

    Update formatting

--- main.ts
+++ main.ts
@@ -3,11 +3,10 @@ const wasmCode = await Deno.readFile(FILE_NAME);
 const wasmModule = new WebAssembly.Module(wasmCode);
 const wasmInstance = new WebAssembly.Instance(wasmModule);
 const {
-	    square,
+  square,
 } = wasmInstance.exports;
 
 console.log(square(1));
 console.log(square(2));
 console.log(square(3));
 console.log(square(4));
-
